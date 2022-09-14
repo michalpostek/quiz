bbaa96a8ec375a075c637bbdbddfc1af6ed178b2
Michał Postek <postek.michal@protonmail.com>
2022-09-14 09:23:45 +0200

    fix LoadingSpinner using process.env.PUBLIC_URL in img src

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -3,8 +3,13 @@ import { StyledLoadingSpinner } from "./LoadingSpinner.styled";
 const LoadingSpinner = () => {
     return (
         <StyledLoadingSpinner>
-            <img src="./logo192.png" alt="React Logo" />
-            <p>Loading</p>
+            <img 
+                src={`${process.env.PUBLIC_URL}/logo192.png`} 
+                alt="React Logo" 
+            />
+            <p>
+                Loading
+            </p>
         </StyledLoadingSpinner>
     );
 }
